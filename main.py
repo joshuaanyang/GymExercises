e4577b8a6c2f202b4be95db4928260a55328ecb0
Joshua Anyang <anyangjoshua@gmail.com>
2022-07-25 09:50:03 +0100

    Build Html

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import requests
-
-headers = {
-	"X-RapidAPI-Key": "bb8f721820msh41917aa6a4592a7p1466e2jsn11d3b1b25ada",
-	"X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
-}
-
-name = "barbell squat"
-url = f"https://exercisedb.p.rapidapi.com/exercises/name/{name}"
-
-response = requests.request("GET", url, headers=headers, params=name)
-
-print(response.text)
+# import requests
+#
+# headers = {
+# 	"X-RapidAPI-Key": "bb8f721820msh41917aa6a4592a7p1466e2jsn11d3b1b25ada",
+# 	"X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+# }
+#
+# name = "barbell squat"
+# url = f"https://exercisedb.p.rapidapi.com/exercises/name/{name}"
+#
+# response = requests.request("GET", url, headers=headers, params=name)
+#
+# print(response.text)
