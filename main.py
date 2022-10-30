166b9d0c3476196f7dab1fd02cb7dbb32a761817
Joshua Anyang <anyangjoshua@gmail.com>
2022-10-30 13:34:00 +0000

    Building backend: remove delay

--- main.py
+++ main.py
@@ -96,6 +96,7 @@ db.create_all()
 def home():
     if request.method == "POST":
         exercise = request.form.get('search')
+        print(exercise)
 
         headers = {
             "X-RapidAPI-Key": rapid_key,
