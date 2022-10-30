29eccaf0cb31431954b1de2873cbd43a2996b952
Joshua Anyang <anyangjoshua@gmail.com>
2022-10-30 18:09:07 +0000

    Building backend: serverside iphone bug

--- main.py
+++ main.py
@@ -106,8 +106,7 @@ def home():
         url = f"https://exercisedb.p.rapidapi.com/exercises/name/{exercise}"
 
         response = requests.request("GET", url, headers=headers)
-        # print(response.read_json())
-        print(json.loads(response.read()))
+        print(response.content)
 
         ex_list = response.json()  # Did not store the search json in a database because it would be repopulated often
         # might add that functionality in a new update
